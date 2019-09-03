43a1e0982476613fc63a539e976fe0cde0c4941a
RohanBipetkar <rohanbipetkar@gmail.com>
2019-09-03 08:26:42 -0800

    controller and model creation

---
+++ secondmicroservice/secondmicroservice/src/main/java/com/example/secondmicroservice/AccountController.java
@@ -0,0 +1,28 @@
+package com.example.secondmicroservice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+	
+	private Map<Integer, Integer> balance = new HashMap<>();
+	
+	@RequestMapping(path = "/accountbalance/{id}")
+	public Integer getAccontBalance(@PathVariable Integer id) {
+		return balance.get(id);
+	}
+	
+	@RequestMapping(path = "/accountbalance", method = RequestMethod.POST,consumes={"application/json","application/xml"})
+	public void addBalance(@RequestBody Profile profile) {
+		balance.put(profile.getAccountNo(), profile.getBalance());
+	}
+	
+	
+}
---
+++ secondmicroservice/secondmicroservice/src/main/java/com/example/secondmicroservice/Profile.java
@@ -0,0 +1,43 @@
+package com.example.secondmicroservice;
+
+
+public class Profile {
+	private Integer accountNo;
+	private String name;
+	private int age;
+	private String gender;
+	private int balance;
+	
+	public Integer getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(Integer accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+}
+
